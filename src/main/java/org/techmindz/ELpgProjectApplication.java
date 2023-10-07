185c4eda56bd5d8330f45824b488a122203ba262
nihal <nihalmurali047@gmail.com>
2023-10-07 16:16:56 +0530

    create the spring boot application and adding the files into git repository

---
+++ src/main/java/org/techmindz/ELpgProjectApplication.java
@@ -0,0 +1,13 @@
+package org.techmindz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELpgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELpgProjectApplication.class, args);
+	}
+
+}
